42ed515378190de649b15a73449e7e483044acae
Xavid <xavid@mit.edu>
2016-09-24 23:49:55 -0400

    * Fix workdir path for new node version. * Make sure all changes to the checkout are committed before syncing workdir.

--- workdir.js
+++ workdir.js
@@ -1,14 +1,18 @@
+var process = require('process');
 var execFile = require('child_process').execFile;
+var svn = require('./svn');
 
-var workdirs_root = "workdirs/";
+var workdirs_root = process.cwd() + "/workdirs/";
 
 exports.get_path = function (repo) {
   return workdirs_root + repo + '/';
 }
 
 exports.sync = function (repo, callback) {
-  execFile(__dirname + '/bin/sync-workdir.sh', [repo],
-           function (error, stdout, stderr) {
-             callback(error);
-           });
+  svn.svn_sync(repo, null, function () {
+    execFile(__dirname + '/bin/sync-workdir.sh', [repo],
+             function (error, stdout, stderr) {
+               callback(error);
+             });
+  });
 }
